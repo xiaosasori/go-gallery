9eb1b3de1d99e789343b2f217b88ebe77b69c8ea
Nguyen Minh Anh <xiaosasori@gmail.com>
2021-03-14 19:56:24 +0900

    add orm

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/schema v1.2.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/lib/pq v1.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/xiaosasori/go-gallery/controllers"
+	"github.com/xiaosasori/go-gallery/models"
+)
+
+const (
+	host     = "arjuna.db.elephantsql.com"
+	port     = 5432
+	user     = "hwrvxfng"
+	password = "RnzDzwy5Jg-1L9tI9hedGV3M1ykyZcpG"
+	dbname   = "hwrvxfng"
 )
 
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.DestructiveReset()
+	// user := models.User{
+	// 	Name:  "MA",
+	// 	Email: "ma@gmail.com",
+	// }
+	// if err := us.Create(&user); err != nil {
+	// 	panic(err)
+	// }
+	user, err := us.ByID(1)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user)
 	staticC := controllers.NewStatic()
-	usersC := controllers.NewUsers()
+	usersC := controllers.NewUsers(us)
 
 	r := mux.NewRouter()
 	r.Handle("/", staticC.Home).Methods("GET")
